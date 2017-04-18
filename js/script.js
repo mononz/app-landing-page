b5d7e26e2bb344831103a617791297fbbbde8dcb
Jared Hall <jared@mononz.com>
2017-04-18 19:24:03 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+// FIX HOME SCREEN HEIGHT
+$(document).ready(function() {
+
+    "use strict";
+
+    setInterval(function() {
+
+        "use strict";
+
+        var widnowHeight = $(window).height();
+        var containerHeight = $(".home-container").height();
+        var padTop = widnowHeight - containerHeight;
+        $(".home-container").css({
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+        });
+    }, 10)
+});
